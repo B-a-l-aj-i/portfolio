57c55928f4c22ca1740f533fe9ce760a6b411153
aj <balajivijay679@gmail.com>
2024-04-26 10:53:34 +0530

    ad

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,16 +7,16 @@ function About(){
         <section id="ab" className="abt">
             <center><p className="p" >About</p></center>
             <div id="about">
-            <article className="content">
+            <div className="content">
             <p>I am an aspiring Frontend developer with a passion for creating user-friendly websites and I am
             quite familiar with some web technologies I am looking for an internship where I can leverage my
             skills and implement my knowledge in the real world. I aim to learn from experienced
             professionals and gain hands-on experience in web development
             </p>
-            </article>
-            <aside className="dp">
+            </div>
+            <div className="dp">
                 <img className="pic" src={pic} alt="pic"/>
-            </aside>
+            </div>
             </div>
         </section>
     )
