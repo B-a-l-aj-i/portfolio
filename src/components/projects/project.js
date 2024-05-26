1f96ccbf04903c33963e5bd3c032e23c54c6fed5
aj <balajivijay679@gmail.com>
2024-05-27 00:40:13 +0530

    c

--- src/components/projects/project.js
+++ src/components/projects/project.js
@@ -1,62 +1,114 @@
-import React, { useState, useEffect, useRef } from 'react';
 import yt from "./assets/yt.png";
 import simon from "./assets/SIMON2.png";
 import ai from "./assets/Screenshot 2024-04-23 211643.png";
 import wai from "./assets/image copy.png";
 import ld from "./assets/image.png";
 import find from "./assets/FIND.png";
+
 import "./project.css";
 
 function Projects() {
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const scrollRef = useRef(null);
+    if (window.innerWidth > 768) {
+        window.addEventListener('scroll', () => {
+            var revel = document.querySelectorAll(".card");
+            var rev = document.querySelector(".container");
+            let head = document.querySelector(".p");
+            for (let i = 0; i < revel.length; i++) {
+                let windowHeight = window.innerHeight;
+                let revelTop = rev.getBoundingClientRect().top;
+                let revelpoint = 20;
 
-    const cards = [
-        { src: yt, desc: "YOUTUBE-CLONE", link: "https://github.com/B-a-l-aj-i/YOUTUBE-CLONE", text: "it not the complete clone of youtube it has only single page in it and responsive todo" },
-        { src: simon, desc: "SIMON-GAME", link: "https://github.com/B-a-l-aj-i/SIMON-GAME", text: "As usual a basic todo-list,you add tasks and remove them and it my first project" },
-        { src: ai, desc: "SEARCH-AI", link: "https://github.com/B-a-l-aj-i", text: "" },
-        { src: wai, desc: "WHERE-AM-I", link: "https://github.com/B-a-l-aj-i/Where-am-I", text: "" },
-        { src: ld, desc: "LUCKY-DICER", link: "https://github.com/B-a-l-aj-i/LuckyDicer", text: "" },
-        { src: find, desc: "FIND-IMAGES", link: "https://github.com/B-a-l-aj-i/find-images", text: "" },
-    ];
-
-    const handleScroll = () => {
-        const scrollLeft = scrollRef.current.scrollLeft;
-        const cardWidth = scrollRef.current.clientWidth;
-        const index = Math.round(scrollLeft / cardWidth);
-        setCurrentIndex(index);
-    };
-
-    useEffect(() => {
-        const ref = scrollRef.current;
-        ref.addEventListener('scroll', handleScroll);
-        return () => ref.removeEventListener('scroll', handleScroll);
-    }, []);
+                if (revelTop < windowHeight - revelpoint) {
+                    head.classList.add('ls');
+                    revel[i].classList.add('add' + i);
+                } else {
+                    head.classList.remove('ls');
+                    revel[i].classList.remove('add' + i);
+                }
+            }
+        });
+    }
 
     return (
         <section className="container" id="projects">
             <div>
                 <center><p className="p ls">Projects</p></center>
             </div>
+
             <div className="allcards-container">
-                <div className="allcards" ref={scrollRef}>
-                    {cards.map((card, index) => (
-                        <div className="card" key={index}>
-                            <div className="cardpic">
-                                <img src={card.src} alt="imgs" />
-                                <article className="cont">{card.text}</article>
-                            </div>
-                            <div className="desc">
-                                <p>{card.desc}</p>
-                                <a href={card.link}><button className="more">visit</button></a>
-                            </div>
+                <div className="allcards">
+                    <div className="card">
+                        <div className="cardpic">
+                            <img src={yt} alt="imgs" />
+                            <article className="cont">
+                                it not the complete clone of youtube it has only single page in it and responsive todo
+                            </article>
                         </div>
-                    ))}
-                </div>
-                <div className="dots">
-                    {cards.map((_, index) => (
-                        <span key={index} className={`dot ${currentIndex === index ? 'active' : ''}`} />
-                    ))}
+                        <div className="desc">
+                            <p>YOUTUBE-CLONE</p>
+                            <a href="https://github.com/B-a-l-aj-i/YOUTUBE-CLONE"><button className="more">visit</button></a>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <div className="cardpic">
+                            <img src={simon} alt="imgs" style={{ objectFit: 'fill' }} />
+                            <article className="cont">
+                                As usual a basic todo-list, you add tasks and remove them and it is my first project
+                            </article>
+                        </div>
+                        <div className="desc">
+                            <p>SIMON-GAME</p>
+                            <a href="https://github.com/B-a-l-aj-i/SIMON-GAME"><button className="more">visit</button></a>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <div className="cardpic">
+                            <img src={ai} alt="imgs" style={{ objectFit: 'fill' }} />
+                            <article className="cont">
+                            </article>
+                        </div>
+                        <div className="desc">
+                            <p>SEARCH-AI</p>
+                            <a href="https://github.com/B-a-l-aj-i"><button className="more">visit</button></a>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="cardpic">
+                            <img src={wai} alt="imgs" />
+                            <article className="cont">
+                            </article>
+                        </div>
+                        <div className="desc">
+                            <p>WHERE-AM-I</p>
+                            <a href="https://github.com/B-a-l-aj-i/Where-am-I"><button className="more">visit</button></a>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <div className="cardpic">
+                            <img src={ld} alt="imgs" />
+                            <article className="cont">
+                            </article>
+                        </div>
+                        <div className="desc">
+                            <p>LUCKY-DICER</p>
+                            <a href="https://github.com/B-a-l-aj-i/LuckyDicer"><button className="more">visit</button></a>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <div className="cardpic">
+                            <img src={find} alt="imgs" style={{ objectFit: 'fill' }} />
+                            <article className="cont">
+                            </article>
+                        </div>
+                        <div className="desc">
+                            <p>FIND-IMAGES</p>
+                            <a href="https://github.com/B-a-l-aj-i/find-images"><button className="more">visit</button></a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
